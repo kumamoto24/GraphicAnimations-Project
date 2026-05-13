8f5da951de0cc453b4ba85ec27eef25f6fade24e
Kevin Hu <huzikun2004@outlook.com>
2026-05-13 18:41:29 +0800

    Sovle task C

--- src/rendering/cameras/PanningCamera.cpp
+++ src/rendering/cameras/PanningCamera.cpp
@@ -54,7 +54,7 @@ void PanningCamera::update(const Window& window, float dt, bool controls_enabled
 
     inverse_view_matrix = glm::inverse(view_matrix);
 
-    projection_matrix = glm::infinitePerspective(fov, window.get_framebuffer_aspect_ratio(), 1.0f);
+    projection_matrix = glm::infinitePerspective(fov, window.get_framebuffer_aspect_ratio(), near); // Solution: C (1/2)
     inverse_projection_matrix = glm::inverse(projection_matrix);
 }
 
