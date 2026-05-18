0c45b12139ee221bd6ba0af5846133be50ad0d1b
Kevin Hu <huzikun2004@outlook.com>
2026-05-18 16:43:10 +0800

    Solution for task G

--- res/shaders/entity/frag.glsl
+++ res/shaders/entity/frag.glsl
@@ -1,8 +1,10 @@
 #version 410 core
 #include "../common/lights.glsl"
 
+// Solution: G (3/11): Shading
 in VertexOut {
-    LightingResult lighting_result;
+    vec3 ws_position;
+    vec3 ws_normal;
     vec2 texture_coordinate;
 } frag_in;
 
@@ -10,13 +12,38 @@ layout(location = 0) out vec4 out_colour;
 
 // Global Data
 uniform float inverse_gamma;
+uniform vec3 ws_view_position;
+
+// Solution: G (4/11): Shading
+// Material properties
+uniform vec3 diffuse_tint;
+uniform vec3 specular_tint;
+uniform vec3 ambient_tint;
+uniform float shininess;
+
+// Light Data
+#if NUM_PL > 0
+layout (std140) uniform PointLightArray {
+    PointLightData point_lights[NUM_PL];
+};
+#endif
 
 uniform sampler2D diffuse_texture;
 uniform sampler2D specular_map_texture;
 
 void main() {
-    // Resolve the per vertex lighting with per fragment texture sampling.
-    vec3 resolved_lighting = resolve_textured_light_calculation(frag_in.lighting_result, diffuse_texture, specular_map_texture, frag_in.texture_coordinate);
+    // Solution: G (5/11): Shading
+    vec3 ws_view_dir = normalize(ws_view_position - frag_in.ws_position);
+    LightCalculatioData light_calculation_data = LightCalculatioData(frag_in.ws_position, ws_view_dir, normalize(frag_in.ws_normal));
+    Material material = Material(diffuse_tint, specular_tint, ambient_tint, shininess);
+
+    LightingResult lighting_result = total_light_calculation(light_calculation_data, material
+        #if NUM_PL > 0
+        ,point_lights
+        #endif
+    );
+
+    vec3 resolved_lighting = resolve_textured_light_calculation(lighting_result, diffuse_texture, specular_map_texture, frag_in.texture_coordinate);
 
     out_colour = vec4(resolved_lighting, 1.0f);
     out_colour.rgb = pow(out_colour.rgb, vec3(inverse_gamma));
